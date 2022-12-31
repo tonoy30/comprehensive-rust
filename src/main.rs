ce9de8e6d7c9221b19e81967c64417f981c83eed
Md. Tonoy Akando <tonoy.sust@gmail.com>
2023-01-01 01:37:50 +0600

    variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+fn main() { // program entry point
+    let mut x: i32 = 6; // mutable variable binding
+    print!("{x}"); // marco for printing
+    while x != 1 { // no parenthesis around expression
+        if x % 2 == 0 { // math like other language
+            x = x / 2;
+        }else {
+            x = 3 * x + 1;
+        }
+        print!(" -> {x}");
+    }
+    println!();
 }
